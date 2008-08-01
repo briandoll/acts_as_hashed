2ae4400919a8cb0309383fdb07eda6fced945204
Brian Doll <brian@emphaticsolutions.com>
2008-08-01 16:44:06 -0700

    Minor code cleanup, adding license.  Ready for release!

--- lib/briandoll/acts_as_hashed.rb
+++ lib/briandoll/acts_as_hashed.rb
@@ -1,6 +1,7 @@
 module BrianDoll
   module ActsAsHashed
     module HashedMethods
+
       def acts_as_hashed(options = {})
         options[:from] ||= :name
         class_inheritable_accessor :acts_as_hashed_options
@@ -15,18 +16,18 @@ module BrianDoll
     
     module InstanceMethods      
       private
+
       def create_encryption_hash
         properties_to_hash = acts_as_hashed_options[:from]
         value_to_hash = ""
         unless properties_to_hash.is_a? Array
           value_to_hash = self.send(properties_to_hash).to_s
         else
-          properties_to_hash.each do |prop|
-            value_to_hash += self.send(prop).to_s
-          end
+          value_to_hash = properties_to_hash.map { |prop| self.send(prop).to_s }.join
         end
         self.encryption_hash = Zlib.crc32(value_to_hash)
       end
+
     end
   end
 end
